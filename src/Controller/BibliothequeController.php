c94d96f28bceb1d6a53272f60e01d37307aa13b7
Ghis.cost <ghis.cost@gmail.com>
2026-01-12 09:06:38 +0100

    changement de sens de l'affichage

--- src/Controller/BibliothequeController.php
+++ src/Controller/BibliothequeController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HtmlSanitizer\HtmlSanitizerInterface;
+use Doctrine\Common\Collections\Criteria;
 
 final class BibliothequeController extends AbstractController
 {
@@ -29,3 +30,38 @@ final class BibliothequeController extends AbstractController
         ]);
     }
 }
+
+// final class BibliothequeController extends AbstractController
+// {
+//     #[Route('/bibliotheque', name: 'app_bibliotheque')]
+//     public function index(
+//         HistoiresRepository $histoiresRepository,
+//         HtmlSanitizerInterface $htmlSanitizer
+//     ): Response
+//     {
+//         $histoires = $histoiresRepository->trouverHistoires();
+
+//         foreach ($histoires as $hist) {
+
+//             // Création du critère de tri (ancien → récent)
+//             $criteria = Criteria::create()
+//                 ->orderBy(['id' => Criteria::ASC]);
+
+            
+//             $chapitresTries = $hist->getChapitres()->matching($criteria);
+
+//             foreach ($chapitresTries as $chapitre) {
+//                 $chapitre->setContenu(
+//                     $htmlSanitizer->sanitize($chapitre->getContenu())
+//                 );
+//             }
+
+//             // on remplace la collection
+//             $hist->setChapitres($chapitresTries);
+//         }
+
+//         return $this->render('bibliotheque/index.html.twig', [
+//             'histoires' => $histoires
+//         ]);
+//     }
+// }
